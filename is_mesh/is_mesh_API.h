9fdbaddddcbeba73f2a0518745a05453e4e0fb38
Asger Nyman Christiansen <asny@dtu.dk>
2013-07-03 15:04:06 +0200

    Added assertions to basic mesh operations.

--- is_mesh/is_mesh_API.h
+++ is_mesh/is_mesh_API.h
@@ -391,6 +391,8 @@ public:
     {
         std::vector<node_key> nodes;
         get_nodes(e, nodes);
+        assert(nodes[0] != NULL_NODE);
+        assert(nodes[1] != NULL_NODE);
         node_key res = mesh.edge_collapse_helper(e, nodes[1], nodes[0]);
         if (res == (node_key)-1) {
             return NULL_NODE;
@@ -405,7 +407,10 @@ public:
         node_key n1 = *lk_e.nodes_begin();
         node_key n2 = split(e);
         edge_key e2 = get_edge(n1, n2);
+        assert(e2 != NULL_EDGE);
         node_key n3 = collapse(e2);
+        assert(n3 != NULL_NODE);
+        assert(n1 == n3);
         return n3;
     }
     
@@ -416,7 +421,10 @@ public:
         node_key n1 = *lk_f.nodes_begin();
         node_key n2 = split(f);
         edge_key e = get_edge(n1, n2);
+        assert(e != NULL_EDGE);
         node_key n3 = collapse(e);
+        assert(n3 != NULL_NODE);
+        assert(n1 == n3);
         return n3;
     }
     
