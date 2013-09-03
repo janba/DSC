3a252e47f5cc65f892efe9b755ec5d9eac0a5fe2
Asger Nyman Christiansen <asny@dtu.dk>
2013-09-02 20:45:57 -0500

    Added copyright notice to attributes.h

--- src/attributes.h
+++ src/attributes.h
@@ -1,7 +1,21 @@
+//
+//  Deformabel Simplicial Complex (DSC) method
+//  Copyright (C) 2013  Technical University of Denmark
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  See licence.txt for a copy of the GNU General Public License.
+
 #ifndef ATTRIBUTES_H
 #define ATTRIBUTES_H
-
-/** 2013 Marek K. Misztal and Mark Viinblad Jensen **/
 
 template<typename MT>
 class default_node_traits
@@ -162,5 +176,4 @@ public:
     
 };
 
-//ATTRIBUTES_H
 #endif
