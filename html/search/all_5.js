8b5222540a16e57bb7adfd61c24e78b9d0c102c9
Morten Nobel-Jørgensen <morten@nobel-joergensen.com>
2015-06-23 12:38:41 +0200

    Update docs

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -4,7 +4,6 @@ var searchData=
   ['face',['Face',['../classis__mesh_1_1_face.html',1,'is_mesh']]],
   ['faceiterator',['FaceIterator',['../classis__mesh_1_1_face_iterator.html',1,'is_mesh']]],
   ['facekey',['FaceKey',['../classis__mesh_1_1_face_key.html',1,'is_mesh']]],
-  ['find',['find',['../classis__mesh_1_1kernel.html#aa97166a31c0188e24e21f8e5398b3166',1,'is_mesh::kernel::find(key_type const &amp;k)'],['../classis__mesh_1_1kernel.html#aab254aaa4a2aa51a8a8684c78b54107c',1,'is_mesh::kernel::find(key_type const &amp;k) const ']]],
   ['find_5fiterator',['find_iterator',['../classis__mesh_1_1kernel.html#ac2b5a1023316589cb6d7bd4dc8d3f6c9',1,'is_mesh::kernel']]],
   ['find_5fvalid_5fiterator',['find_valid_iterator',['../classis__mesh_1_1kernel.html#ada53efd8efab1e89ddeff815e697d51d',1,'is_mesh::kernel']]],
   ['first_5fbit',['first_bit',['../class_c_g_l_a_1_1_bit_mask.html#aa8cfc3ea57875dd564bc47d12d83082d',1,'CGLA::BitMask']]]
--- html/search/all_8.js
+++ html/search/all_8.js
@@ -3,7 +3,7 @@ var searchData=
   ['identity_5fquatd',['identity_Quatd',['../class_c_g_l_a_1_1_quatd.html#a8fd9861066b54574d1dd54ffc690bb96',1,'CGLA::Quatd']]],
   ['identity_5fquatf',['identity_Quatf',['../class_c_g_l_a_1_1_quatf.html#a326a770162b2a773e4bdcc14f4383132',1,'CGLA::Quatf']]],
   ['import_5fgeometry',['import_geometry',['../namespaceis__mesh.html#a79f48e5232a4f0f87bee9521e598d023',1,'is_mesh']]],
-  ['import_5fsurface_5fmesh',['import_surface_mesh',['../namespaceis__mesh.html#a9ac1b9514888d5e220aa5384e5c02cb6',1,'is_mesh']]],
+  ['import_5fsurface_5fmesh',['import_surface_mesh',['../namespaceis__mesh.html#a53e78d51266b164346a26a3a09207473',1,'is_mesh']]],
   ['import_5ftet_5fmesh',['import_tet_mesh',['../namespaceis__mesh.html#a40aabc6e06db0fc38af39c9810ab2c8d',1,'is_mesh']]],
   ['import_5fvoxel_5fgrid',['import_voxel_grid',['../namespaceis__mesh.html#a5c85e45bbd0f43445f6800177d7c8f82',1,'is_mesh']]],
   ['insert_5fedge',['insert_edge',['../classis__mesh_1_1_i_s_mesh.html#a55c3dca4c71079243cf1c2348dbf49f9',1,'is_mesh::ISMesh']]],
@@ -12,12 +12,13 @@ var searchData=
   ['insert_5ftetrahedron',['insert_tetrahedron',['../classis__mesh_1_1_i_s_mesh.html#a251a5f317eec2777bbf97bd2a4fef125',1,'is_mesh::ISMesh']]],
   ['intersection_5fwith_5flink',['intersection_with_link',['../class_d_s_c_1_1_deformable_simplicial_complex.html#aba29293c9de62520f192a6f9e10dd705',1,'DSC::DeformableSimplicialComplex']]],
   ['inverse',['inverse',['../class_c_g_l_a_1_1_arith_quat.html#ac9cff0d205ea0f7ee125d23b46e7381a',1,'CGLA::ArithQuat']]],
+  ['is_5fexcluded',['is_excluded',['../classis__mesh_1_1kernel.html#a219c734cfcc5f06b13ef5781dc5a33b8',1,'is_mesh::kernel']]],
   ['is_5finverted',['is_inverted',['../classis__mesh_1_1_i_s_mesh.html#aea1d4ac840ed8d4179c19a18ad69b863',1,'is_mesh::ISMesh']]],
   ['is_5fmesh',['is_mesh',['../namespaceis__mesh.html',1,'']]],
   ['is_5fmotion_5ffinished',['is_motion_finished',['../class_d_s_c_1_1_velocity_func.html#ae55ac05da9cd237b7ef78937de5b21c4',1,'DSC::VelocityFunc']]],
   ['is_5fsafe_5feditable',['is_safe_editable',['../class_d_s_c_1_1_deformable_simplicial_complex.html#a32b37a793d11c6b7c39ac25ae33275fc',1,'DSC::DeformableSimplicialComplex::is_safe_editable(const is_mesh::NodeKey &amp;nid)'],['../class_d_s_c_1_1_deformable_simplicial_complex.html#a2cde5d2f57188a152ceefce165251185',1,'DSC::DeformableSimplicialComplex::is_safe_editable(const is_mesh::EdgeKey &amp;eid)'],['../class_d_s_c_1_1_deformable_simplicial_complex.html#a4a14e78c32769287426f774626c6f016',1,'DSC::DeformableSimplicialComplex::is_safe_editable(const is_mesh::FaceKey &amp;fid)'],['../class_d_s_c_1_1_deformable_simplicial_complex.html#aafcf441b12ee9ec99b4fa21f0a93fc07',1,'DSC::DeformableSimplicialComplex::is_safe_editable(const is_mesh::TetrahedronKey &amp;tid)']]],
   ['is_5funsafe_5feditable',['is_unsafe_editable',['../class_d_s_c_1_1_deformable_simplicial_complex.html#ad4b3a945d7ba1ff26a361a612ad90211',1,'DSC::DeformableSimplicialComplex::is_unsafe_editable(const is_mesh::NodeKey &amp;nid)'],['../class_d_s_c_1_1_deformable_simplicial_complex.html#a37c8076695009df88c9534c24e57b85d',1,'DSC::DeformableSimplicialComplex::is_unsafe_editable(const is_mesh::EdgeKey &amp;eid)'],['../class_d_s_c_1_1_deformable_simplicial_complex.html#ac53319e65d297838945b82d12697e295',1,'DSC::DeformableSimplicialComplex::is_unsafe_editable(const is_mesh::FaceKey &amp;fid)'],['../class_d_s_c_1_1_deformable_simplicial_complex.html#a208c40f44a35c1148c73fea98bf9800d',1,'DSC::DeformableSimplicialComplex::is_unsafe_editable(const is_mesh::TetrahedronKey &amp;tid)']]],
-  ['is_5fvalid',['is_valid',['../classis__mesh_1_1kernel.html#a0807fed05950749cb8527e9ceec2924f',1,'is_mesh::kernel']]],
+  ['is_5fvalid',['is_valid',['../classis__mesh_1_1kernel.html#adcd2a6ad8fbda9671b6fe3a36f25bc3e',1,'is_mesh::kernel']]],
   ['ismesh',['ISMesh',['../classis__mesh_1_1_i_s_mesh.html',1,'is_mesh']]],
   ['util',['util',['../namespaceis__mesh_1_1util.html',1,'is_mesh']]]
 ];
